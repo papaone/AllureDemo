9258ef0ddf25f103961e547406a0bb70cb992076
papaone <vitaliybugayev@gmail.com>
2021-08-15 21:00:35 +0300

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Screenshots;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.google.common.io.Files;
@@ -6,26 +7,30 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.apache.commons.io.FileUtils;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.PageLoadStrategy;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.io.File;
 import java.io.IOException;
 
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+
 public class BaseTest {
     @BeforeAll
-    static void setupAllureReports() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true).savePageSource(true));
+    public static void openApp() {
+        Configuration.browser = "chrome";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserVersion = "88";
+        Configuration.timeout = 10000;
+        Configuration.headless = false;
+        Configuration.browserCapabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities.setCapability("enableVNC", true);
+        Configuration.pageLoadStrategy = PageLoadStrategy.NONE.toString();
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(false));
     }
 
     @AfterEach
-    public void tearDown() throws IOException {
-        screenshot();
-    }
-
-    @Attachment(value = "{0}", type = "image/png")
-    public byte[] screenshot() throws IOException {
-        File screenshot = Screenshots.takeScreenShotAsFile();
-        FileUtils.copyFile(screenshot, new File("target\\allure-results\\screenshots\\" + screenshot.getName()));
-        return Files.toByteArray(screenshot);
+    public void tearDown() {
+        closeWebDriver();
     }
 }
